56f0119e025b8b91b990d4c55cf707f613b53a3d
Ashen Udithamal <udithamal.lk@gmail.com>
2021-03-15 22:30:15 +0530

    backup clean added to cron

--- config/backup.php
+++ config/backup.php
@@ -116,7 +116,7 @@ return [
              * The disk names on which the backups will be stored.
              */
             'disks' => [
-                'local','one_drive',
+                'one_drive',
             ],
         ],
 
--- cron.bat
+++ cron.bat
@@ -1,3 +1,4 @@
 cd C:\xampp\htdocs\tealeaves
 
-C:\xampp\php\php.exe artisan backup:run 1>> NUL 2>&1
+C:\xampp\php\php.exe artisan backup:run --disable-notifications 1>> NUL 2>&1
+C:\xampp\php\php.exe artisan backup:clean --disable-notifications 1>> NUL 2>&1
